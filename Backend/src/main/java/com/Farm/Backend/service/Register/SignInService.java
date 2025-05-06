85f7c2d29ff1c8dd052aa9702b177bb9de321824
Sameer-Shekhar-code <ssgnhss@gmail.com>
2025-05-07 01:43:01 +0530

    Register Functionality created

---
+++ Backend/src/main/java/com/Farm/Backend/service/Register/SignInService.java
@@ -0,0 +1,33 @@
+package com.Farm.Backend.service.Register;
+
+import com.Farm.Backend.entity.Users;
+import com.Farm.Backend.repo.UsersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignInService {
+
+    @Autowired
+    UsersRepo usersRepo;
+
+    public String register(String username, String password) {
+        // Check if user already exists
+        if (usersRepo.existsByUsername(username)) {
+            return "User with username '" + username + "' already exists.";
+        }
+
+        // Create new user
+        Users newUser = new Users();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+
+        // Save to database
+        Users savedUser = usersRepo.save(newUser);
+
+        // Return or log saved user details
+        return "User registered: ID = " + savedUser.getUser_id() +
+                ", Username = " + savedUser.getUsername();
+    }
+}
+
